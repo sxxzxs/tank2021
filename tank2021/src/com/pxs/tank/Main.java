f1b8a84aa5e493ab2d5addf7be7820a2cb5cc07d
pxs <pxs@LAPTOP-8DMJ3CH1>
2021-03-09 17:37:14 +0800

    初步认识Frame类，从Frame类中继承并重写paint方法，将坦克坐标定义为变量，添加键盘监听事件

---
+++ tank2021/src/com/pxs/tank/Main.java
@@ -0,0 +1,14 @@
+package com.pxs.tank;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		TankFrame tf = new TankFrame();
+		
+	}
+	
+}
---
+++ tank2021/src/com/pxs/tank/TankFrame.java
@@ -0,0 +1,57 @@
+package com.pxs.tank;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+//继承是为了重写paint方法
+public class TankFrame extends Frame {
+	int x = 200, y = 200;
+	
+	public TankFrame() {
+		//Frame f = new Frame();		//新建一窗口,
+		setVisible(true);		//将窗口设为可见
+		setSize(800,600);
+		setResizable(false); 	//窗口大小不可变
+		setTitle("tank war");	//标题栏名称
+		
+		this.addKeyListener(new MyKeyListener());	//添加键盘监听
+		
+		//添加window监听器，监听windowClosing,当点击窗口拔插就会触发windowClosing
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}			
+		});
+	}
+	
+	//窗口重新绘制时候自动调用paint
+	@Override
+	public void paint(Graphics g) {
+		g.fillRect(x, y, 50, 50);	//填充一个正方形
+		x += 10;
+		y += 10;
+	}
+
+	class MyKeyListener extends KeyAdapter{
+
+		//一个键被按下去时候调用
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("..");
+		}
+		
+		//一个键被抬起来时候调用
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println(".");
+		}
+		
+		
+	}
+}
